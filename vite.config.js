a33b866c172512c58aa73f863c616535d96d3df1
sergioDavidCano <sergio0201david@gmail.com>
2024-02-27 20:37:01 -0500

    deploy en react y git hub

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portafolio-sergio-c/',
   build: {
     chunkSizeWarningLimit: 1600,
   },
